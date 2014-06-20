c2764821bce8365d8d48532b4cd156ad99abf732
David Simmer <david@simmerdesigns.com>
2014-06-19 19:07:47 -0500

    add IE version classes to html element

--- templates/head.php
+++ templates/head.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
-<html class="no-js" <?php language_attributes(); ?>>
+<!--[if lt IE 8]> <html class="no-js lt-ie10 lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie10 lt-ie9 ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 9]> <html class="ie9 lt-ie10" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 10]> <html class="ie10" <?php language_attributes(); ?>> <![endif]-->
+<!--[if (gt IE 10)|!(IE)]> <html <?php language_attributes(); ?>> <![endif]-->
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,5 +13,8 @@
 
   <?php wp_head(); ?>
 
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+  <![endif]-->
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
